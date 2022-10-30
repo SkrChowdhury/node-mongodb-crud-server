65fef646c9db24e618109a5458426fd906b61843
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-10-31 00:45:17 +0600

    final commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,30 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+//user = dbuser1
+// password = ECtQOXnV8WNxWStu
+
+const uri =
+  'mongodb+srv://user_skr:bKCoPGH9qaEHccqW@cluster0.1dqilwu.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    const userCollection = client.db('nodeMongoCrud').collection('users');
+
+    app.post('/users', (req, res) => {
+      const user = req.body;
+      console.log(user);
+    });
+  } finally {
+  }
+}
+
+run().catch((err) => console.error(err));
 
 app.get('/', (req, res) => {
   res.send('Hello from node mongo crud server');
